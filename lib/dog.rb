b440128b8882967389630ae784913ad91493169f
Talisa Erazo <talisaerazo@gmail.com>
2018-10-31 23:26:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
+
 class Dog
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
+  def initialize(breed)
+    @breed = breed
   end
 
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 end
